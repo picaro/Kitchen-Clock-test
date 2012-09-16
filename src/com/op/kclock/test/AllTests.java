adbb7ff0b87d473f4f415138948637f8307feb66
Alexander Pastukhov <picaro.vs@gmail.com>
2012-09-16 22:12:32 +0300

    sorting test

--- src/com/op/kclock/test/AllTests.java
+++ src/com/op/kclock/test/AllTests.java
@@ -8,7 +8,7 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(AllTests.class.getName());
 		//$JUnit-BEGIN$
-//		suite.addTestSuite(MainActTest.class);
+		suite.addTestSuite(MainActTest.class);
 		suite.addTestSuite(SortingTest.class);
 //		suite.addTestSuite(AlarmingTest.class);
 //		suite.addTestSuite(HistoryAndLogsTest.class);
--- src/com/op/kclock/test/SortingTest.java
+++ src/com/op/kclock/test/SortingTest.java
@@ -56,50 +56,47 @@ public class SortingTest extends ActivityInstrumentationTestCase2<MainActivity> 
 	public void testSorting() {
 		assertNotNull(activity);
 		mInstrumentation.waitForIdleSync();
-		
-		
-		//settings to default
-		 SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		 Editor editor = prefs.edit();
-	     editor.clear();
-	     editor.commit();//add three alarms
-		
-		//check unsorted
-	    MainActTest.addAlarm(activity, 2, 55, 0);
-	    MainActTest.addAlarm(activity, 1, 55, 0);
+
+		// settings to default
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		Editor editor = prefs.edit();
+		editor.clear();
+		editor.commit();// add three alarms
+
+		// check unsorted
+		MainActTest.addAlarm(activity, 2, 55, 0);
+		MainActTest.addAlarm(activity, 1, 55, 0);
 		TimePickDialog tPicker = activity.getTimePickDialog();
 		MainActTest.addAlarm(activity, 3, 0, 0);
-	    solo.sleep(1000);
-	    assertEquals(2, activity.getAlarmList().get(0).getHour());
-		//change to runned first
-	     prefs.edit().putString("pref_sortlist_key", "smallfirst");
-	 	//ListPreference pickstyle = (ListPreference) prefs.findPreference("pref_sortlist_key");
+		solo.sleep(1000);
+		assertEquals(2, activity.getAlarmList().get(0).getHour());
+		// change to runned first
+		editor.putString(context.getString(R.string.pref_sortlist_key),
+				MainActivity.SMALLFIRST);
+		editor.commit();
+		String sortType = prefs.getString(
+				context.getString(R.string.pref_sortlist_key),
+				MainActivity.UNSORTED);
+		assertEquals(MainActivity.SMALLFIRST, sortType);
 
-	     
-	     MainActTest.addAlarm(activity, 4, 0, 0);
-		
-	     final AlarmClock alarm = activity.getAlarmList().get(1);
-	     MainActTest.tapOnAlarm(activity, 1, AlarmClock.TimerState.PAUSED);	
-	     mInstrumentation.waitForIdleSync();
+		mInstrumentation.waitForIdleSync();
+		assertEquals(1, activity.getAlarmList().get(0).getHour());
+		MainActTest.addAlarm(activity, 0, 30, 0);
+		mInstrumentation.waitForIdleSync();
+		assertEquals(0, activity.getAlarmList().get(0).getHour());
 
-			
-	     assertEquals(1, activity.getAlarmList().get(0).getHour());
-			 
-		//add alarms
-		
-		//change to nearest first
-		//add alarms
-				
+		final AlarmClock alarm = activity.getAlarmList().get(0);
+		mInstrumentation.waitForIdleSync();
+
 		dbHelper.close();
 		solo.finishOpenedActivities();
 	}
 
-
-
 	@Override
 	public void tearDown() throws Exception {
 		super.tearDown();
-		if (dbHelper != null) 
+		if (dbHelper != null)
 			dbHelper.close();
 		if (solo != null)
 			solo.finishOpenedActivities();
